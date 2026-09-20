c2304f6ed6f74b74c304f07924c7f9781676994b
yasher3413 <yashgandhi2023@gmail.com>
2026-09-20 16:20:50 -0400

    chore: scaffold Tauri 2 + React/TypeScript/Vite project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main className="app-shell">
+      <h1>twig</h1>
+      <p>Browser chrome goes here.</p>
+    </main>
+  );
+}
+
+export default App;
